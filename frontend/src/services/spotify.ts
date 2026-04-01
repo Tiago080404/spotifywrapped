0e7eac07e1042dce1e087ef76270816131198d16
ilias.teknis <ilias.teknis@db-n.com>
2026-04-01 12:06:15 +0200

    fix: lint errors

--- frontend/src/services/spotify.ts
+++ frontend/src/services/spotify.ts
@@ -16,9 +16,12 @@ export const getTopSongs = async (token: string, timeRange: string = 'medium_ter
   return data.items
 }
 export const getTopGenres = async (token: string, timeRange: string = 'medium_term') => {
-  const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
+  const response = await fetch(
+    `https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    },
+  )
   const data = await response.json()
   console.log(data.items[0].genres)
 }
