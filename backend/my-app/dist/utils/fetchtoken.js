32d4ab45236db9aedeba47b841a4ef23af991979
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2026-04-07 12:05:16 +0200

    feat: implement caching and Spotify token exchange functionality

---
+++ backend/my-app/dist/utils/fetchtoken.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+const redirect_uri = "http://127.0.0.1:5173/dashboard";
+export async function fetchSpotifyToken(code) {
+    const res = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${Buffer.from(`${process.env.CLIENTID}:${process.env.CLIENTSECRET}`).toString("base64")}`,
+        },
+        body: new URLSearchParams({
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: redirect_uri,
+        }),
+    });
+    return res.json();
+}
